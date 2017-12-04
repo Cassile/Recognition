c1ef0ee103c85f8893a9f6b02e3da5ee2c9dba43
Cassile <525071874@qq.com>
2017-12-04 16:36:15 -0600

    Return Array
    In the perfrm recognition, return array

--- performRecognition.py
+++ performRecognition.py
@@ -10,6 +10,8 @@ from PIL import Image
 import scipy.misc
 Label = ['0','1','2','3','4','5','6','7','8','9',')','=','-','+','*','/','(']
 def recognize(fn):
+    result = []
+    i = 0
     clf, pp = joblib.load("digits_cls.pkl")
     # My image is a 200x374 jpeg that is 102kb large
     foo = Image.open(fn)
@@ -44,7 +46,7 @@ def recognize(fn):
     #rects = [cv2.boundingRect(ctr) for ctr in ctrs]
     for ctr in ctrs:
         area = cv2.contourArea(ctr)
-        if area>10:
+        if area>50:
             # Draw the rectangles
             rect = cv2.boundingRect(ctr)
             cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
@@ -73,49 +75,16 @@ def recognize(fn):
             roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
             nbr = clf.predict(roi_hog_fd)
             #f.write(str(int(nbr[0])))
-            print nbr
+            result[i]=int(nbr[0])
+            i=i+1
             cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
-
-
-    # For each rectangular region, calculate HOG features and predict
-    # the digit using Linear SVM.
-    #f = open('helloworld.txt', 'wb')
-    #for rect in rects:
-        # # Draw the rectangles
-        # cv2.rectangle(im, (rect[0], rect[1]), (rect[0] + rect[2], rect[1] + rect[3]), (0, 255, 0), 3)
-        # # Make the rectangular region around the digit
-        # leng = int(rect[3] * 1.6)
-        # pt1 = int(rect[1] + rect[3] // 2 - leng // 2)
-        # pt2 = int(rect[0] + rect[2] // 2 - leng // 2)
-        # roi = im_th[pt1:pt1 + leng, pt2:pt2 + leng]
-        # try:
-        #     roi = cv2.resize(roi, (28, 28), interpolation=cv2.INTER_AREA)
-        # except:
-        #     try:
-        #         roi = cv2.resize(np.rot90(roi, axes=(-1, -2)), (28, 28), interpolation=cv2.INTER_AREA)
-        #     except:
-        #         roi = cv2.resize(np.rot90(roi, axes=(-2, -1)), (28, 28), interpolation=cv2.INTER_AREA)
-        #     finally:
-        #         cv2.putText(im, "Fail", (rect[0], rect[1]), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
-        #         break
-        #
-        #
-        # roi = cv2.dilate(roi, (3, 3))
-        # # Calculate the HOG features
-        # roi_hog_fd = hog(roi, orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
-        # # clf=pp.fit([roi_hog_fd], 'float64')	scaler.scale_ = scaler.std_.copy()
-        # # pp.scale_= pp.std_.copy()
-        # roi_hog_fd = pp.transform(np.array([roi_hog_fd], 'float64'))
-        # nbr = clf.predict(roi_hog_fd)
-        # #f.write(str(int(nbr[0])))
-        # cv2.putText(im, str(int(nbr[0])), (rect[0], rect[1]), cv2.FONT_HERSHEY_DUPLEX, 2, (0, 255, 255), 3)
-
-    #f.close()
     cv2.namedWindow("Resulting Image with Rectangular ROIs", cv2.WINDOW_NORMAL)
     cv2.imshow("Resulting Image with Rectangular ROIs", im)
     #scipy.misc.imsave(fn, im)
     cv2.waitKey()
+    return  result
 
-# if __name__ == '__main__':
-#     sys.exit(recognize(sys.argv[1]))
-recognize("1.jpg")
+
+if __name__ == '__main__':
+    sys.exit(recognize(sys.argv[1]))
+#srecognize("1.jpg")
